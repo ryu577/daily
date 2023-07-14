1386bc8df196c82b465ae6392f1e84b41b98558c
Rohit Pandey <rohitpandey@Rohits-MacBook-Air.local>
2023-07-14 00:41:22 -0700

    Added mesh counting

---
+++ 2307/230710.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pyray.shapes.fourd.tesseract_graph as tg
+from itertools import combinations
+
+
+# Check that an adjacency list is a valid mesh.
+tf = tg.TsrctFcGraph(angle=np.pi/2)
+tf.dfs_flatten2('00-+')
+tf.reset_vert_col()
+tf.actually_flatten()
+tf.mk_xy_set()
+
+
+if len(tf.xy_set) == 24:
+    print("valid mesh")
+else:
+    print("invalid mesh")
+
+
+# Get the adjacency list
+for i in range(32):
+    for j in range(3):
+        for k in combinations(np.arange(32),23):
+            print("comb")
+
