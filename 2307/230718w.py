b7350197097b5eb935a80bb749ae9641e2b90d93
Rohit Pandey <rohitpandey576@gmail.com>
2023-07-19 13:31:19 -0700

    Update 230718w.py

--- 2307/230718w.py
+++ 2307/230718w.py
@@ -4,9 +4,18 @@ p = 0.6
 a = np.array([4,3,3,2,1])
 
 a = sorted(a, reverse=True)
+n = sum(a)
 
-
-def get_av(a, ix, k, n):
+def get_av(a, ix, k):
+    """
+    Gets the availablity percentage of a system where
+    replicas are placed in pods, the replicas form a k-of-n system
+    and the pods (racks) have some probability of failing. 
+    Inputs:
+        a: Array telling you how many replicas are in each pod (rack).
+        ix: Just pass 0 to the function always.
+        k: The number of replicas that should be up for the system to function.
+    """
     pod = a[ix]
     if k <= 0:
         return 1
