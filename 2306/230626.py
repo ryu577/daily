05bfeb0cbde97129739daeb12b1f1b66d4a4109d
ryu577 <rohitpandey576@gmail.com>
2023-08-07 10:29:03 -0700

    New file back from June

--- 2306/230626.py
+++ 2306/230626.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.stats import binom
 
 
-def est_n(sla=.997, n=40, p=0.0527293873732649):
+def est_n(sla=.99, n=40, p=0.17):
     n1 = n
     sf = binom.sf(n-1, n1, p)
     while sf < sla:
@@ -13,9 +13,11 @@ def est_n(sla=.997, n=40, p=0.0527293873732649):
 
 n = 40
 n1 = est_n(n=n)
-n2 = est_n(n=n, sla=.997, p=.99)
-discnt_fctr = 1-n2/n1
+n2 = est_n(n=n, sla=.99, p=.8)
+discnt_fctr = 1-(n1+(n2-n1)*.1)/n1
 print(discnt_fctr)
+
+
 ##########################################
 ## Now some pyray stuff.
 
